b0aa3f84c1b8e0533ef2135cf454270f93619be0
Bosco Basabana <basabanansita@gmail.com>
2019-11-26 20:53:44 +0100

    resolve

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const http = require('http');
-const express = require('express');
-//const app  = require('./app');
-const app  = express()
-app.use(express.static('public'));
+const app  = require('./app');
+
 
 const port = process.env.PORT || 3000;
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
 server.listen(port, ()=> { console.log(`server up and running on Port ${port}`);});
